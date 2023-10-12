62bb654c40c49fb0b9a7c5f2d850cdeacb5915aa
Sufian Ashraf <sajidashraf098@gmai.com>
2023-10-12 21:06:37 +0600

    Add board size as a variable

--- helpers.py
+++ helpers.py
@@ -4,37 +4,34 @@ import random
 class Conway:
     """Conway's Game of Life model"""
 
-    board_size = 10
-
-    def __init__(self):
+    def __init__(self, size):
+        self.size = size
         self.board = self.random_board()
 
     def __str__(self):
-        size = self.board_size
-
         string_board = ""
-        for i in range(size):
-            for j in range(size):
+        for i in range(self.size):
+            for j in range(self.size):
                 string_board += str(self.board[i][j]) + " "
             string_board += "\n"
 
         return string_board.rstrip()
 
     def random_board(self):
-        size = self.board_size
-        return [[random.choice([1, 0]) for _ in range(size)] for _ in range(size)]
+        return [
+            [random.choice([1, 0]) for _ in range(self.size)] for _ in range(self.size)
+        ]
 
-    def exclusive_grid_sum(self, x, y):
-        size = self.board_size
+    def neighbours(self, x, y):
         ex_sum = -self.board[x][y]
         for i in range(-1, 2):
             for j in range(-1, 2):
-                ex_sum += self.board[(x + i) % size][(y + j) % size]
+                ex_sum += self.board[(x + i) % self.size][(y + j) % self.size]
 
         return ex_sum
 
     def update_state(self, x, y):
-        neighbors = self.exclusive_grid_sum(x, y)
+        neighbors = self.neighbours(x, y)
 
         if self.board[x][y] == 1 and 2 <= neighbors <= 3:
             self.board[x][y] = 1
@@ -44,13 +41,13 @@ class Conway:
             self.board[x][y] = 0
 
     def update_board(self):
-        for i in range(self.board_size):
-            for j in range(self.board_size):
+        for i in range(self.size):
+            for j in range(self.size):
                 self.update_state(i, j)
 
 
 def test() -> None:
-    conway = Conway()
+    conway = Conway(10)
     print(conway)
     conway.update_board()
     print("\n\n\n")
--- prototype.py
+++ prototype.py
@@ -2,12 +2,15 @@ from turtle import Turtle
 from helpers import Conway
 import time
 
+BOARD_SIZE = 20
+
+
 def main():
-    conway = Conway()
+    conway = Conway(BOARD_SIZE)
     turtle = Turtle()
     turtle.speed(0)
     turtle.hideturtle()
-    turtle.screen.bgcolor('black')
+    turtle.screen.bgcolor("black")
     while True:
         render_board(conway, turtle)
         conway.update_board()
@@ -19,18 +22,17 @@ def render_board(conway, turtle: Turtle):
     dimensions = turtle.screen.screensize()
     turtle.penup()
     turtle.goto(-dimensions[0], dimensions[1])
-    step = dimensions[0] / conway.board_size
-    
-    for i in range(conway.board_size):
-        for j in range(conway.board_size):
+    step = dimensions[0] / BOARD_SIZE
+
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
             if conway.board[i][j] == 1:
                 turtle.pendown()
-                turtle.dot(step, 'yellow')
+                turtle.dot(step, "yellow")
                 turtle.penup()
             turtle.forward(step)
         p = turtle.position()
-        turtle.goto(p[0] - step * conway.board_size, p[1] - step)
-
+        turtle.goto(p[0] - step * BOARD_SIZE, p[1] - step)
 
     turtle.screen.update()
     time.sleep(0.3)
